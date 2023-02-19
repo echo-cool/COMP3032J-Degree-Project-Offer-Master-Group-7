81d1d26e66f4f6f1fd66141124f4739deb2c8c60
Michael-QinTong <73060940+Michael-QinTong@users.noreply.github.com>
2023-02-19 18:33:23 +0800

    Result code

---
+++ src/main/java/com/group7/common/ResultCode.java
@@ -0,0 +1,17 @@
+package com.group7.common;
+
+
+
+/**
+ * @author xujinfengxu
+ */
+public class ResultCode {
+    /**
+     * 成功
+     */
+    public static Integer SUCCESS = 20000;
+    /**
+     * 失败
+     */
+    public static Integer ERROR = 20001;
+}
