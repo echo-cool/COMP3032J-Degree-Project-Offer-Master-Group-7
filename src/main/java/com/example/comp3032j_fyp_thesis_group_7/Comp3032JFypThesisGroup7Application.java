aa2139c4a34512f43752d99ffbc49f6c37350d19
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-19 18:04:53 +0800

    add README

---
+++ src/main/java/com/example/comp3032j_fyp_thesis_group_7/Comp3032JFypThesisGroup7Application.java
@@ -0,0 +1,13 @@
+package com.example.comp3032j_fyp_thesis_group_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3032JFypThesisGroup7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3032JFypThesisGroup7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comp3032j_fyp_thesis_group_7/Comp3032JFypThesisGroup7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp3032j_fyp_thesis_group_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3032JFypThesisGroup7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
