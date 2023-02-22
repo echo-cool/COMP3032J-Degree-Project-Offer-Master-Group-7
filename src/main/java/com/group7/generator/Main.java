12de8c5440301dee4816339d97c8b0f41c6d1e8e
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-22 19:01:34 +0800

    project update

--- src/main/java/com/group7/generator/Main.java
+++ src/main/java/com/group7/generator/Main.java
@@ -1,8 +1,27 @@
-package com.group7.generator;/**
+package com.group7.generator;
+
+/**
  * @Author: WangYuyang
  * @Date: 2023/2/22-18:42
  * @Project: COMP3032J_FYP_Thesis_Group_7
  * @Package: com.group7.generator
- * @Description: 
- **/public class Main {
+ * @Description:
+ **/
+
+import org.mybatis.generator.api.ShellRunner;
+
+/**
+ *
+ * @author wangmengjun
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String config = Main.class.getClassLoader()
+                .getResource("generatorConfig.xml").getFile();
+        String[] arg = { "-configfile", config, "-overwrite" };
+        ShellRunner.main(arg);
+    }
 }
