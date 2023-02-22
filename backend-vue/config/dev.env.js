eaf137f160729d5924d81d477724b9e3fabe8497
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-22 19:55:40 +0800

    project update

--- backend-vue/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080/"',
-  OSS_PATH: ' "https://qintong-edu.oss-cn-beijing.aliyuncs.com"'
-})
