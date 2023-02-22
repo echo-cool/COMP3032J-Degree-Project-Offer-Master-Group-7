ac9aaa644716a31a3181f41cfb483e35915f2605
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-22 18:59:44 +0800

    project update

---
+++ src/main/java/com/group7/generator/Main.java
@@ -0,0 +1,8 @@
+package com.group7.generator;/**
+ * @Author: WangYuyang
+ * @Date: 2023/2/22-18:42
+ * @Project: COMP3032J_FYP_Thesis_Group_7
+ * @Package: com.group7.generator
+ * @Description: 
+ **/public class Main {
+}
