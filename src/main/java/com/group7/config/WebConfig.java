daf2b289211629aaadc4419c89ec1374bf6161f9
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-27 13:09:43 +0800

    update

--- src/main/java/com/group7/config/WebConfig.java
+++ src/main/java/com/group7/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.group7.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 所有方法都会处理跨域请求
+                .allowedOrigins("*")    // 允许请求的域名
+                .allowedHeaders("*")    // 允许的请求头
+                .allowedMethods("GET","POST");  // 允许的请求方式
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //添加拦截器
