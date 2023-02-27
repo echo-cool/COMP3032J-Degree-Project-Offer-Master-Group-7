b78cfa44412359fb9a5a88efde25b8468c058daf
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-02-27 12:42:54 +0800

    update

--- src/main/java/com/group7/controller/user/UserController.java
+++ src/main/java/com/group7/controller/user/UserController.java
@@ -1,32 +1,46 @@
 package com.group7.controller.user;
+
 import com.group7.db.mappers.UserMapper;
 import com.group7.db.model.User;
 import com.group7.db.model.UserExample;
 import com.group7.utils.common.R;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
 
 
-
 @RestController
-@RequestMapping("user")
+@RequestMapping("/secure")
 public class UserController {
 
     @Resource
     private UserMapper userMapper;
 
-    @GetMapping("/{id}")
-    public R selectOne(@PathVariable("id") String id) {
+    @GetMapping("/user")
+    public R getUser(HttpServletRequest request) {
+        String id = request.getAttribute("id").toString();
         UserExample userExample = new UserExample();
         userExample.createCriteria().andIdEqualTo(Integer.valueOf(id));
         User user = userMapper.selectByExample(userExample).get(0);
-        if (user != null){
+        if (user != null) {
             return R.ok().data("data", user);
-        }
-        else {
+        } else {
             return R.error();
         }
+    }
+    @RequestMapping("/getUserInfo")
+    public R getUserInfo(HttpServletRequest request) {
+        String id = request.getAttribute("id").toString();
+        String username = request.getAttribute("username").toString();
+        String openid = request.getAttribute("openid").toString();
+        String email = request.getAttribute("email").toString();
+        String role = request.getAttribute("role").toString();
+        return R.ok()
+                .data("id", id)
+                .data("username", username)
+                .data("openid", openid)
+                .data("email", email)
+                .data("role", role);
     }
 
 }
