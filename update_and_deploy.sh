eb251547cc1a7e646da930a1bd52ecaf209c943f
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-03-21 23:30:07 +0800

    update_deploy.sh

--- update_and_deploy.sh
+++ update_and_deploy.sh
@@ -5,5 +5,6 @@ git reset --hard origin/main
 docker pull ghcr.io/echo-cool/fyp/fyp_offer_master_springboot:latest
 docker pull ghcr.io/echo-cool/fyp/fyp_offer_master_vue_user:latest
 docker pull ghcr.io/echo-cool/fyp/fyp_offer_master_vue_admin:latest
+docker pull ghcr.io/echo-cool/fyp/fyp_offer_master_springboot_admin:latest
 docker-compose down
 docker-compose up -d
