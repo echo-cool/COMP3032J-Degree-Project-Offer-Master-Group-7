7674751792aea0b57cd66bf20d3e447dba5c7e68
Luca <zhe.liu1@ucdconnect.ie>
2023-03-24 20:03:13 +0800

    homepage - popular programs section frontend requests

---
+++ vue-user/src/api/program.js
@@ -0,0 +1,13 @@
+import request from "@/utils/request"
+
+export default{
+
+    // get top ranked schools using specific rank rule
+    getPopularPrograms(degree, limit){
+        return request({
+            url: `/api/program/public/  /${degree}/${limit}`,
+            method: 'get'
+        })
+    }
+
+}
