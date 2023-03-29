2c409e5c47668e51848e059d69c327b4ede5ebd2
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-03-30 00:32:50 +0800

    update

--- django_ml/django_ml/settings.py
+++ django_ml/django_ml/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-USE_X_FORWARDED_HOST = True
+# USE_X_FORWARDED_HOST = True
 FORCE_SCRIPT_NAME = '/django/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
