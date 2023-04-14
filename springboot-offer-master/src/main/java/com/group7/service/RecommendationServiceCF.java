70fb9184dd5ed787899d9a41dd754b23a91f8a77
MaZixoa <zixiao.ma@ucdconnect.ie>
2023-04-14 17:47:41 +0800

    cf

--- springboot-offer-master/src/main/java/com/group7/service/RecommendationServiceCF.java
+++ springboot-offer-master/src/main/java/com/group7/service/RecommendationServiceCF.java
@@ -2,10 +2,13 @@ package com.group7.service;
 
 import com.group7.db.jpa.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface RecommendationServiceCF {
 
     public List<User> similarityCalculate(User user);
+
+    public List<long []> getData();
 
 }
--- springboot-offer-master/src/main/java/com/group7/service/impl/RecommendationServiceCFImpl.java
+++ springboot-offer-master/src/main/java/com/group7/service/impl/RecommendationServiceCFImpl.java
@@ -1,16 +1,11 @@
 package com.group7.service.impl;
 
-import com.group7.db.jpa.Profile;
-import com.group7.db.jpa.User;
-import com.group7.db.jpa.UserRepository;
+import com.group7.db.jpa.*;
 import com.group7.service.RecommendationServiceCF;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class RecommendationServiceCFImpl implements RecommendationServiceCF {
@@ -18,8 +13,37 @@ public class RecommendationServiceCFImpl implements RecommendationServiceCF {
     @Resource
     UserRepository userRepository;
 
+    @Resource
+    ApplicationRepository applicationRepository;
+
+    @Resource
+    ProgramRepository programRepository;
+
     @Override
     public List<User> similarityCalculate(User user) {
+        Set<Application> applications = user.getApplications();
+        List<User> allUsers = userRepository.findAll();
+
+        allUsers.removeIf(filter -> Objects.equals(filter.getId(), user.getId()));
+
+        Map<User, Double> userScore = new HashMap<>();
+
+
+
+        return null;
+    }
+
+    @Override
+    public List<long[]> getData() {
+        List<long[]> result = new ArrayList<>();
+        List<Application> applications = applicationRepository.findAll();
+
+        for (Application application: applications) {
+            result.add(new long[]{application.getUser().getId(), application.getProgram().getId(), 1});
+        }
+
+
+
         return null;
     }
 }
