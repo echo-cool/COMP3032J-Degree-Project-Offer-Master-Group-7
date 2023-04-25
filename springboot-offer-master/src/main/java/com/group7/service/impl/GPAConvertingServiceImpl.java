169ba4f7c78fd44fafe0ee1bb2033a8b4a92e8b6
Luca <zhe.liu1@ucdconnect.ie>
2023-04-25 16:33:44 +0800

    EasyExcel service bug fixed

--- springboot-offer-master/src/main/java/com/group7/service/impl/GPAConvertingServiceImpl.java
+++ springboot-offer-master/src/main/java/com/group7/service/impl/GPAConvertingServiceImpl.java
@@ -4,12 +4,14 @@ import com.alibaba.excel.EasyExcel;
 import com.group7.entitiy.excel.GradeData;
 import com.group7.listener.GPADataListener;
 import com.group7.service.GPAConvertingService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @Author: LiuZhe
  * @Date: 2023/4/25 - 14:04
  */
+@Service
 public class GPAConvertingServiceImpl implements GPAConvertingService {
 
     @Override
