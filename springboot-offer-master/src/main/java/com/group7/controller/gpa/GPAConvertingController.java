8612b89c44b81c1b4496aff35dfc4f912f9be75e
Luca <zhe.liu1@ucdconnect.ie>
2023-04-25 17:59:30 +0800

    download GPA converting template backend

--- springboot-offer-master/src/main/java/com/group7/controller/gpa/GPAConvertingController.java
+++ springboot-offer-master/src/main/java/com/group7/controller/gpa/GPAConvertingController.java
@@ -3,13 +3,20 @@ package com.group7.controller.gpa;
 import com.group7.service.GPAConvertingService;
 import com.group7.utils.common.R;
 import jakarta.annotation.Resource;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.net.URLEncoder;
 
 /**
  * @Author: LiuZhe
@@ -27,6 +34,42 @@ public class GPAConvertingController {
     public R convertGPA(MultipartFile file){
 
         gpaConvertingService.convertGPA(file, gpaConvertingService);
+
+        return R.ok();
+    }
+
+    @RequestMapping("/download-template")
+    public R downloadTemplate(HttpServletResponse response) {
+        FileInputStream fis = null;
+        ServletOutputStream sos = null;
+        try {
+            String fileName = "GPA_Convert_Template.xlsx";
+            // resources下路径，比如文件位置在：resources/file/test.docx
+            String path = "static/excel/" + fileName;
+            // 设置响应头
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+            ClassPathResource classPathResource = new ClassPathResource(path);
+            fis = new FileInputStream(classPathResource.getFile());
+            sos = response.getOutputStream();
+            IOUtils.copy(fis, sos);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Download failed！");
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (sos != null) {
+                    sos.flush();
+                    sos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return R.ok();
     }
