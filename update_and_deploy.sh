add6f1ba096e0a39f3ac0d8cccc377bcd8571ffe
Yuyang Wang <yuyang.wang821@gmail.com>
2023-04-25 20:03:08 +0800

    Updated At: Tue Apr 25 20:03:08 CST 2023

--- update_and_deploy.sh
+++ update_and_deploy.sh
@@ -2,9 +2,8 @@ git checkout main
 git fetch --all
 git reset --hard origin/main
 
-
+sudo docker-compose down --remove-orphans
 sudo docker-compose pull
-sudo docker-compose down
 sudo docker-compose up -d --no-build --remove-orphans
 
 #docker image prune -a --force
