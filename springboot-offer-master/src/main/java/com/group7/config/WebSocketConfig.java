17ac93612b99f7526f61fd28fc8db3ae3be10570
Michael-QinTong <73060940+Michael-QinTong@users.noreply.github.com>
2023-04-25 15:21:10 +0800

    config of websocket

---
+++ springboot-offer-master/src/main/java/com/group7/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.group7.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Author:
+ * @Explain: 开启webSocket
+ * @Params:
+ * @Return
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
