32d6a3d6a9b15adec8ee68fe813aeda24def58cb
Luca <zhe.liu1@ucdconnect.ie>
2023-04-25 22:46:11 +0800

    gpa convert reset bug fixed

--- springboot-offer-master/src/main/java/com/group7/db/jpa/GradeRepository.java
+++ springboot-offer-master/src/main/java/com/group7/db/jpa/GradeRepository.java
@@ -1,5 +1,6 @@
 package com.group7.db.jpa;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * @Author: LiuZhe
  * @Date: 2023/4/25 - 21:48
  */
+@Transactional
 public interface GradeRepository extends JpaRepository<Grade, Long>, PagingAndSortingRepository<Grade, Long>, CrudRepository<Grade, Long> {
 
     void deleteAllByUser(User user);
