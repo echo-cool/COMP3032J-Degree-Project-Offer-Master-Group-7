135c3ad00df5a879fada388d3b00ca1f06772a7b
Luca <zhe.liu1@ucdconnect.ie>
2023-04-26 18:07:12 +0800

    GPA converting report data header class

---
+++ springboot-offer-master/src/main/java/com/group7/entitiy/excel/GPAReportData.java
@@ -0,0 +1,36 @@
+package com.group7.entitiy.excel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+/**
+ * @Author: LiuZhe
+ * @Date: 2023/4/26 - 17:56
+ */
+@Data
+@EqualsAndHashCode
+public class GPAReportData {
+
+    @ExcelProperty(value = "", index = 0)
+    private int order;
+
+    @ExcelProperty(value = "Course Name", index = 1)
+    private String courseName;
+
+    @ExcelProperty(value = "Credits", index = 2)
+    private double credits;
+
+    @ExcelProperty(value = "Grade", index = 3)
+    private String grade;
+
+    @ExcelProperty(value = "US Grade", index = 4)
+    private String gradeUS;
+
+    @ExcelProperty(value = "US Grade Points", index = 5)
+    private String gradePointsUS;
+
+
+}
