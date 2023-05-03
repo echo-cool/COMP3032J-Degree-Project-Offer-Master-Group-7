988ba6d8e2953514acb98b2d752ec5dd92d76003
Luca <zhe.liu1@ucdconnect.ie>
2023-05-04 01:20:53 +0800

    pagination on program explore page, connected to the backend query

--- vue-user/src/components/myComp/homePageComp/ExploreFilterPrograms.vue
+++ vue-user/src/components/myComp/homePageComp/ExploreFilterPrograms.vue
@@ -10,6 +10,7 @@
                 </div>
             </div>
 
+            <!-- filter bar -->
             <div class="default-exp-wrapper">
                 <div class="inner">
                     <div class="filter-select-option">
@@ -88,6 +89,7 @@
                 </div>
             </div>
 
+            <!-- program card area -->
             <div class="row g-5"
                  data-sal="slide-up"
                  data-sal-delay="150"
@@ -118,6 +120,21 @@
                 </div>
                 <!--                <h3 v-if="!programs.length" class="text-center">No Match Found</h3>-->
             </div>
+
+            <!-- pagination -->
+            <nav class="pagination-wrapper" aria-label="Page navigation example">
+                <paginate
+                    :page-count="totalPage"
+                    :page-range="3"
+                    :margin-pages="2"
+                    :click-handler="pageClickCallback"
+                    :prev-text="'Prev'"
+                    :next-text="'Next'"
+                    :container-class="'pagination'"
+                    :page-class="'page-item'"
+                >
+                </paginate>
+            </nav>
         </div>
     </div>
 </template>
@@ -130,6 +147,8 @@ import ProductFilterMixin from '@/mixins/ProductFilterMixin'
 import programApi from "@/api/program";
 import ProgramCard from "@/components/myComp/program/ProgramCard.vue";
 import LikeMixin from "@/mixins/user/LikeMixin";
+import Paginate from "vuejs-paginate-next";
+
 
 export default {
     name: 'ExploreFilterPrograms',
@@ -137,7 +156,8 @@ export default {
         ProgramCard,
         GPARangeSlider,
         NiceSelect,
-        ProductCard
+        ProductCard,
+        Paginate
     },
     props: {
         "query": {
@@ -148,10 +168,10 @@ export default {
             type: Number,
             default: -1
         },
-        "current": {
-            type: Number,
-            default: 1
-        },
+        // "current": {
+        //     type: Number,
+        //     default: 1
+        // },
         currentUser: {}
     },
     mixins: [ProductFilterMixin, LikeMixin],
@@ -165,7 +185,22 @@ export default {
                 major: "all",
                 query: ""
             },
-            programs: []
+            programs: [],
+
+            currPage: 1,
+            totalItemCount: 0,
+            // countOfPage: 10,         // number of items in a page
+        }
+    },
+    computed: {
+        filteredRows() {
+            return this.programs;
+        },
+        pageStart() {
+            return (this.currPage - 1) * this.limit;
+        },
+        totalPage() {
+            return Math.ceil(this.totalItemCount / this.limit);
         }
     },
     created() {
@@ -200,12 +235,23 @@ export default {
         getProgramsByQuery() {
             console.log(this.programQuery)
             // call the api method
-            programApi.getProgramsByQuery(this.programQuery, this.limit, this.current)
+            programApi.getProgramsByQuery(this.programQuery, this.limit, this.currPage)
                 .then(response => {
                     // update the program list
                     this.programs = response.data.programs.content;
+                    this.totalItemCount = response.data.totalElements;
+
+                    // for test
+                    console.log("response total elements: " + this.totalItemCount);
                 })
         },
+
+        pageClickCallback (pageNum) {
+            this.currPage = pageNum;
+            window.scrollTo(0, 0);
+            // request the programs again using new page
+            this.getProgramsByQuery();
+        }
 
         // // get a list of ids of programs that the user liked
         // getLikedPrograms(){
@@ -233,3 +279,19 @@ export default {
     }
 }
 </script>
+
+
+<style>
+nav .active a {
+    background: var(--color-primary) !important;
+    color: var(--color-white) !important;
+}
+
+nav li a:hover {
+    cursor: pointer;
+}
+
+nav li.disabled a{
+    background: var(--color-lighter) !important;
+}
+</style>
