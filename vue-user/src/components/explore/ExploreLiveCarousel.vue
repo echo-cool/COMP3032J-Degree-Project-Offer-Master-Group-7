cecd477e4e9e65f35e124cbb75dc309ab2d61df0
MaZixoa <zixiao.ma@ucdconnect.ie>
2023-05-03 11:57:14 +0800

    carousel_bug_fix

--- vue-user/src/components/explore/ExploreLiveCarousel.vue
+++ vue-user/src/components/explore/ExploreLiveCarousel.vue
@@ -89,6 +89,8 @@ export default {
         }
       },
       programs: [],
+      selectedPrograms: [],
+      selectedProgramIDs: [],
       toggle: true,
       buttonIcon: '<svg class="carousel__icon" viewBox="0 0 24 24" role="img" ariaLabel="arrowRight"><title>arrowRight</title><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path></svg>'
     }
