991757b00ed2216123d135203d672e613906c219
Luca <zhe.liu1@ucdconnect.ie>
2023-05-05 20:46:41 +0800

    loading bug fixed

--- vue-user/src/views/user/GPAConverting.vue
+++ vue-user/src/views/user/GPAConverting.vue
@@ -95,7 +95,7 @@
                             <div v-if="isLoading">
                                 <div  class="d-flex justify-content-center">
                                     <div class="spinner-border" role="status" style="zoom: 5">
-                                        <span class="sr-only">Loading...</span>
+
                                     </div>
                                 </div>
                                 <div class="m-5 d-flex justify-content-center">
