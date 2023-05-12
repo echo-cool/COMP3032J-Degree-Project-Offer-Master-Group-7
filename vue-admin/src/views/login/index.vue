24662b2d3db80619d1dcff01f17c1d9f1300787c
Michael-QinTong <73060940+Michael-QinTong@users.noreply.github.com>
2023-05-13 02:05:15 +0800

    admin login

--- vue-admin/src/views/login/index.vue
+++ vue-admin/src/views/login/index.vue
@@ -47,7 +47,7 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
-      <div style="position:relative">
+      <!-- <div style="position:relative">
         <div class="tips">
           <span>Username : admin</span>
           <span>Password : any</span>
@@ -60,7 +60,7 @@
         <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
           Or connect with
         </el-button>
-      </div>
+      </div> -->
     </el-form>
 
     <el-dialog title="Or connect with" :visible.sync="showDialog">
@@ -99,8 +99,8 @@ export default {
     }
     return {
       loginForm: {
-        email: 'me2@echo.cool',
-        password: '111'
+        email: '',
+        password: ''
       },
       loginRules: {
         email: [{ required: true, trigger: 'blur', validator: validateUsername }],
